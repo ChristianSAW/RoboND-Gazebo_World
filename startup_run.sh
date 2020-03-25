2c1f06336fff59554610a9e8c4fa4a27c6fc12e0
Christian Welling <csw73@cornell.edu>
2020-03-25 00:36:17 +0000

    Adding file structure

---
+++ startup_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run this when starting in the udacity worspace to ensure workspace has # the latest gazebo features and is up to date.  
+
+sudo apt-get update && sudo apt-get upgrade -y
